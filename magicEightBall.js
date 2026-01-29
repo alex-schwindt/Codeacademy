9c19c66cd6ca4b138edb32e7380d4886e82a9722
Alex Schwindt <101961254+alex-schwindt@users.noreply.github.com>
2026-01-28 19:09:23 -0600

    Refactor Magic 8 Ball logic to use switch statement

--- magicEightBall.js
+++ magicEightBall.js
@@ -4,31 +4,31 @@ let randomNumber = Math.floor(Math.random()*8);
 let eightBall;
 (typeof userName === 'string') ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
 console.log(`${userName}: ${userQuestion}`);
-if(randomNumber === 0){
+switch(randomNumber){
+  case 0:
   eightBall = 'It is certain';
+  break;
+  case 1:
+  eightBall = 'It is decidedly so';
+  break;
+  case 2:
+  eightBall = 'Reply hazy try again'
+  break;
+  case 3:
+  eightBall = 'Cannot predict now';
+  break;
+  case 4:
+  eightBall = 'Do not count on it';
+  break;
+  case 5:
+  eightBall = 'My sources say no';
+  break;
+  case 6:
+  eightBall = 'Outlook not so good';
+  break;
+  case 7:
+  eightBall = 'Signs point to yes';
+  break;
 }
-else if (randomNumber === 1){
-   eightBall = 'It is decidedly so';
-}
-else if (randomNumber === 2){
-   eightBall = 'It is decidedly so';
-}
-else if (randomNumber === 3){
-   eightBall = 'Reply hazy try again';
-}
-else if (randomNumber === 4){
-   eightBall = 'Cannot predict now';
-}
-else if (randomNumber === 5){
-   eightBall = 'Do not count on it';
-}
-else if (randomNumber === 6){
-   eightBall = 'My sources say no';
-}
-else if (randomNumber === 7){
-   eightBall = 'Outlook not so good';
-}
-else {
-   eightBall = 'Signs point to yes';
-}
+
 console.log(`Magic 8 Ball Says: ${eightBall}.`)
